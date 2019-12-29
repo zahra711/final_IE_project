7b1226903e4f240831c1d8bb966034c2068db5e6
zahra711 <zahrasalehii711@gmail.com>
2019-12-29 22:51:19 +0330

    final_ie_project

---
+++ app.js
@@ -0,0 +1,40 @@
+function sh1(){
+    var location = {lat: 35.806727, lng: 51.429569};
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: location});
+    var marker= new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+function sh2(){
+    var location = {lat: 35.701387, lng: 51.404887};
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: location});
+    var marker= new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+function sh3(){
+    var location = {lat: 35.729162, lng: 51.378875};
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: location});
+    var marker= new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+function sh4(){
+    var location = {lat: 35.729399 , lng: 51.526800};
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: location});
+    var marker= new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
+function sh5(){
+    var location = {lat: 35.707715 , lng: 51.375946};
+    var map = new google.maps.Map(document.getElementById("map"), {zoom: 17, center: location});
+    var marker= new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
